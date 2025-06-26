0b4c45b644eec5105eb255c943a075773d59dab1
Akshatj26-github <202201185@daiict.ac.in>
2025-06-26 17:17:07 +0530

    Corrected the Allowed origins to live frontend link

--- backend/settings.py
+++ backend/settings.py
@@ -162,8 +162,5 @@ EMAIL_TIMEOUT = None
 
 
 CORS_ALLOWED_ORIGINS = [
-"http://localhost:5173",
-"http://localhost:5174",
-"http://localhost:5175",
-"http://localhost:3000"
+"https://codenest-p4s3.onrender.com/"
 ]
