f7393164e3918cb31f1d45aeeaa1105bf59d052d
Akshatj26-github <202201185@daiict.ac.in>
2025-07-08 23:29:17 +0530

    Fix settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'codenest_db',        
-        'USER': 'akshat',                
-        'PASSWORD': 'akshatjoshi',  
-        'HOST': 'localhost',
-        'PORT': '3306',               
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
